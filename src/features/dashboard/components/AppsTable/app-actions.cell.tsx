c6c5cfc7714b197eb16cc69d6073f0a51fb3eb08
Agrim Jain <agrimjain@Agrim-Jains-Mac-FQ22G9XXP9-Standard.local>
2024-05-06 17:11:11 +0400

    Edit Delete App

--- src/features/dashboard/components/AppsTable/app-actions.cell.tsx
+++ src/features/dashboard/components/AppsTable/app-actions.cell.tsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import { LabelPairedPenSmRegularIcon, LabelPairedTrashSmRegularIcon } from '@deriv/quill-icons';
-import CustomTooltip from '@site/src/components/CustomTooltip';
-import clsx from 'clsx';
+import React, { useState } from 'react';
 import styles from './cells.module.scss';
+import { LabelPairedPenSmRegularIcon, LabelPairedTrashSmRegularIcon } from '@deriv/quill-icons';
+import DeleteAppDialog from '../Dialogs/DeleteAppDialog';
+import UpdateAppDialog from '../Dialogs/UpdateAppDialog';
+import CustomTooltip from '@site/src/components/CustomTooltip';
 
-type TAppActionsCellProps = {
-  openDeleteDialog: () => void;
-  openEditDialog: () => void;
-  flex_end?: boolean;
-};
+const AppActionsCell = () => {
+  const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [isUpdateDialogOpen, setUpdateDialogOpen] = useState(false);
 
-const AppActionsCell = ({
-  openDeleteDialog,
-  openEditDialog,
-  flex_end = false,
-}: TAppActionsCellProps) => {
+  const handleDeleteClick = () => {
+    setDeleteDialogOpen(true);
+  };
+
+  const handleUpdateClick = () => {
+    setUpdateDialogOpen(true);
+  };
+
   return (
-    <div
-      className={clsx(styles.appActions, { [styles.flex_end]: flex_end })}
-      data-testid={'app-action-cell'}
-    >
-      <span onClick={openEditDialog} data-testid={'update-app-button'}>
+    <div className={styles.appActions} data-testid={'app-action-cell'}>
+      <div onClick={handleUpdateClick} data-testid={'update-app-button'}>
         <CustomTooltip text='Edit application details'>
           <LabelPairedPenSmRegularIcon />
         </CustomTooltip>
-      </span>
-
-      <span onClick={openDeleteDialog} data-testid={'delete-app-button'}>
-        <CustomTooltip text='Delete application'>
+      </div>
+      <div onClick={handleDeleteClick} data-testid={'delete-app-button'}>
+        <CustomTooltip text='Delete application details'>
           <LabelPairedTrashSmRegularIcon />
         </CustomTooltip>
-      </span>
+      </div>
+      {isDeleteDialogOpen && (
+        <DeleteAppDialog onClose={() => setDeleteDialogOpen(false)} appId={0} />
+      )}
+      {isUpdateDialogOpen && (
+        <UpdateAppDialog onClose={() => setUpdateDialogOpen(false)} app={undefined} />
+      )}
     </div>
   );
 };

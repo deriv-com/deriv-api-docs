ab7c2e5459912ce3b9abc8b1450f59f2e347cb27
Agrim Jain <agrimjain@Agrim-Jains-Mac-FQ22G9XXP9-Standard.local>
2024-05-07 15:21:53 +0400

    delete popup

--- src/features/dashboard/components/AppsTable/app-actions.cell.tsx
+++ src/features/dashboard/components/AppsTable/app-actions.cell.tsx
@@ -1,29 +1,18 @@
 import React, { useState } from 'react';
 import styles from './cells.module.scss';
-import { LabelPairedPenSmRegularIcon, LabelPairedTrashSmRegularIcon } from '@deriv/quill-icons';
+import { LabelPairedTrashSmRegularIcon } from '@deriv/quill-icons';
 import DeleteAppDialog from '../Dialogs/DeleteAppDialog';
-import UpdateAppDialog from '../Dialogs/UpdateAppDialog';
 import CustomTooltip from '@site/src/components/CustomTooltip';
 
 const AppActionsCell = () => {
   const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  const [isUpdateDialogOpen, setUpdateDialogOpen] = useState(false);
 
   const handleDeleteClick = () => {
     setDeleteDialogOpen(true);
   };
 
-  const handleUpdateClick = () => {
-    setUpdateDialogOpen(true);
-  };
-
   return (
     <div className={styles.appActions} data-testid={'app-action-cell'}>
-      <div onClick={handleUpdateClick} data-testid={'update-app-button'}>
-        <CustomTooltip text='Edit application details'>
-          <LabelPairedPenSmRegularIcon />
-        </CustomTooltip>
-      </div>
       <div onClick={handleDeleteClick} data-testid={'delete-app-button'}>
         <CustomTooltip text='Delete application details'>
           <LabelPairedTrashSmRegularIcon />
@@ -31,9 +20,6 @@ const AppActionsCell = () => {
       </div>
       {isDeleteDialogOpen && (
         <DeleteAppDialog onClose={() => setDeleteDialogOpen(false)} appId={0} />
-      )}
-      {isUpdateDialogOpen && (
-        <UpdateAppDialog onClose={() => setUpdateDialogOpen(false)} app={undefined} />
       )}
     </div>
   );

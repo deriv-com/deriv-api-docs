f822c5d2afd5aad1337917314eb7cd7b10fb1e7e
shafin-deriv <shafin@regentmarkets.com>
2024-05-14 11:52:55 +0800

    chore: add custom mobile sidebar

---
+++ src/theme/Navbar/MobileSidebar/PrimaryMenu/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useThemeConfig, ErrorCauseBoundary } from '@docusaurus/theme-common';
+import { splitNavbarItems, useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
+import NavbarItem from '@theme/NavbarItem';
+import './primary-menu.scss';
+
+export function useNavbarItems() {
+  // TODO temporary casting until ThemeConfig type is improved
+  return useThemeConfig().navbar.items;
+}
+
+export default function CustomMobileSidebar() {
+  const mobileSidebar = useNavbarMobileSidebar();
+  const items = useNavbarItems();
+  const [leftItems] = splitNavbarItems(items);
+  console.log('test mobileSidebar', mobileSidebar);
+  console.log('test items', items);
+
+  return (
+    <React.Fragment>
+      {leftItems.map((item, i) => (
+        <ErrorCauseBoundary
+          key={i}
+          onError={(error) =>
+            new Error(
+              `A theme navbar item failed to render.
+        Please double-check the following navbar item (themeConfig.navbar.items) of your Docusaurus config:
+        ${JSON.stringify(item, null, 2)}`,
+            )
+          }
+        >
+          <NavbarItem
+            {...item}
+            onClick={() => {
+              mobileSidebar.toggle();
+            }}
+          />
+        </ErrorCauseBoundary>
+      ))}
+      <div className='navbar__item navbar__link'>Language Switcher Placeholder</div>
+    </React.Fragment>
+  );
+}

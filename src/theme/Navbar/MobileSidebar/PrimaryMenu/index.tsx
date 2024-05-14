4a741fec149fad9ef3c37b25bb9f0844c0888583
shafin-deriv <shafin@regentmarkets.com>
2024-05-14 12:50:22 +0800

    chore: add functionality

--- src/theme/Navbar/MobileSidebar/PrimaryMenu/index.tsx
+++ src/theme/Navbar/MobileSidebar/PrimaryMenu/index.tsx
@@ -3,6 +3,7 @@ import { useThemeConfig, ErrorCauseBoundary } from '@docusaurus/theme-common';
 import { splitNavbarItems, useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
 import NavbarItem from '@theme/NavbarItem';
 import './primary-menu.scss';
+import { Button } from '@deriv/ui';
 
 export function useNavbarItems() {
   // TODO temporary casting until ThemeConfig type is improved
@@ -10,32 +11,53 @@ export function useNavbarItems() {
 }
 
 export default function CustomMobileSidebar() {
+  const [languageSidebarVisible, setLanguageSidebarVisible] = React.useState(false);
   const mobileSidebar = useNavbarMobileSidebar();
   const items = useNavbarItems();
   const [leftItems] = splitNavbarItems(items);
 
+  const toggleLanguageSidebar = () => {
+    setLanguageSidebarVisible(!languageSidebarVisible);
+  };
+
   return (
     <React.Fragment>
-      {leftItems.map((item, i) => (
-        <ErrorCauseBoundary
-          key={i}
-          onError={(error) =>
-            new Error(
-              `A theme navbar item failed to render.
-        Please double-check the following navbar item (themeConfig.navbar.items) of your Docusaurus config:
-        ${JSON.stringify(item, null, 2)}`,
-            )
-          }
-        >
-          <NavbarItem
-            {...item}
-            onClick={() => {
-              mobileSidebar.toggle();
-            }}
-          />
-        </ErrorCauseBoundary>
-      ))}
-      <div className='navbar__item navbar__link'>Language Switcher Placeholder</div>
+      <div>
+        {leftItems.map((item, i) => (
+          <ErrorCauseBoundary
+            key={i}
+            onError={(error) =>
+              new Error(
+                `A theme navbar item failed to render.
+            Please double-check the following navbar item (themeConfig.navbar.items) of your Docusaurus config:
+            ${JSON.stringify(item, null, 2)}`,
+              )
+            }
+          >
+            <NavbarItem
+              {...item}
+              onClick={() => {
+                mobileSidebar.toggle();
+              }}
+            />
+          </ErrorCauseBoundary>
+        ))}
+      </div>
+      <div className='navbar__item navbar__link' onClick={toggleLanguageSidebar}>
+        Language Switcher Placeholder
+      </div>
+
+      {/* Conditionally render the language sidebar */}
+      <div className={`language_sidebar ${languageSidebarVisible ? 'visible' : ''}`}>
+        <Button onClick={toggleLanguageSidebar} className='language_sidebar__buttons'>
+          Back
+        </Button>
+        <div className='language_sidebar__items'>
+          <div>En</div>
+          <div>Fr</div>
+          <div>Th</div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }

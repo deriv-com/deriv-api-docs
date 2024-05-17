bf67442a65040044b6c66e53469447a516dfdc6d
Agrim Jain <agrimjain@Agrim-Jains-Mac-FQ22G9XXP9-Standard.local>
2024-05-17 11:46:53 +0400

    fix: copy button modal

--- src/features/dashboard/components/Dialogs/TokenCreationDialogSuccess/index.tsx
+++ src/features/dashboard/components/Dialogs/TokenCreationDialogSuccess/index.tsx
@@ -1,14 +1,18 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Button, Modal } from '@deriv/ui';
 import styles from './token-creation-dialog-sucess.module.scss';
+import useApiToken from '@site/src/hooks/useApiToken';
 
 type ITokenCreationDialogSuccessProps = {
   setToggleModal: React.Dispatch<React.SetStateAction<boolean>>;
+  inputTokenName: string;
 };
 
 export const TokenCreationDialogSuccess = ({
   setToggleModal,
 }: ITokenCreationDialogSuccessProps) => {
+  const { tokens } = useApiToken();
+
   const onOpenChange = useCallback(
     (open: boolean) => {
       if (!open) {
@@ -17,9 +21,12 @@ export const TokenCreationDialogSuccess = ({
     },
     [setToggleModal],
   );
+
   const handleToggle = () => {
     setToggleModal(false);
   };
+
+  const latestToken = tokens && tokens.length > 0 ? tokens[0] : null;
 
   return (
     <Modal defaultOpen onOpenChange={onOpenChange}>
@@ -27,13 +34,20 @@ export const TokenCreationDialogSuccess = ({
         <div className='modal-overlay'>
           <Modal.Overlay />
           <Modal.PageContent
-            title={'Token created successfully'}
+            title={'Token created successfully!'}
             has_close_button
             className={styles.wrapper}
           >
             <div className={styles.modal}>
-              <p>Your API token is ready to be used.</p>
+              <p>
+                Please save this token key. For security reasons, it can&apos;t be viewed or copied
+                again. If you lose this key, you&apos;ll need to generate a new token.
+              </p>
             </div>
+
+            <span className={styles.textField}>
+              {latestToken && latestToken?.scopes?.includes('admin') && latestToken.token}
+            </span>
 
             <div className={styles.buttonWrapper}>
               <Button color='primary' onClick={handleToggle} className={styles.btn}>

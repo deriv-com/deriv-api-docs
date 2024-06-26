da0ef24cd93ed3a2cf94e5355740eb5f1cf74982
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-06-26 12:47:43 +0800

    chore: change alert to console

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -11,7 +11,7 @@ import { GetStarted } from './GetStarted/GetStarted';
 import RenderOfficialContents from '@site/src/components/RenderOfficialContents';
 
 export default function HomepageFeatures() {
-  alert('HomepageFeatures');
+  console.log('HomepageFeatures');
   return (
     <main className={styles.features}>
       <RenderOfficialContents>

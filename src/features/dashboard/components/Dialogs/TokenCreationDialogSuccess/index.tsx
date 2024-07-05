bb90d49445442d765e1ea96a72b18d3273ab1c99
shafin-deriv <shafin@regentmarkets.com>
2024-07-05 14:41:47 +0800

    chore: add missing translations

--- src/features/dashboard/components/Dialogs/TokenCreationDialogSuccess/index.tsx
+++ src/features/dashboard/components/Dialogs/TokenCreationDialogSuccess/index.tsx
@@ -3,6 +3,7 @@ import { Button, Modal } from '@deriv/ui';
 import styles from './token-creation-dialog-sucess.module.scss';
 import useApiToken from '@site/src/hooks/useApiToken';
 import CopyButton from '../../ApiTokenTable/CopyButton';
+import Translate from '@docusaurus/Translate';
 
 type ITokenCreationDialogSuccessProps = {
   setToggleModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -43,23 +44,29 @@ export const TokenCreationDialogSuccess = ({
         <div className='modal-overlay'>
           <Modal.Overlay />
           <Modal.PageContent className={styles.wrapper}>
-            <div className={styles.title}>Token created successfully!</div>
+            <div className={styles.title}>
+              <Translate>Token created successfully!</Translate>
+            </div>
             <div className={styles.modal}>
               <p>
-                Please save this token key. For security reasons, it can&apos;t be viewed or copied
-                again. If you lose this key, you&apos;ll need to generate a new token.
+                <Translate>
+                  Please save this token key. For security reasons, it can&apos;t be viewed or
+                  copied again. If you lose this key, you&apos;ll need to generate a new token.
+                </Translate>
               </p>
             </div>
             <div className={styles.textField}>
               <div>
-                <div className={styles.key}>Key</div>
+                <div className={styles.key}>
+                  <Translate>Key</Translate>
+                </div>
                 {latestToken}
               </div>
               <CopyButton value={latestToken} has_admin={false} />
             </div>
             <div className={styles.button_wrapper}>
               <Button color='primary' onClick={handleToggle} className={styles.btn}>
-                OK
+                <Translate>OK</Translate>
               </Button>
             </div>
           </Modal.PageContent>

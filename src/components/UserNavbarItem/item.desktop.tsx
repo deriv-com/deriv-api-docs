bfe84fdec505c9b9470d5cd4b943ececcbf50fd5
Thisyahlen <thisyahlen@regentmarkets.com>
2024-10-11 13:28:12 +0800

    feat: implement oidc for login

--- src/components/UserNavbarItem/item.desktop.tsx
+++ src/components/UserNavbarItem/item.desktop.tsx
@@ -3,15 +3,57 @@ import clsx from 'clsx';
 import React, { useState } from 'react';
 import AccountSwitcher from '../AccountSwitcher';
 import { IUserNavbarItemProps } from './item.types';
+import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
 import styles from './UserNavbarItem.module.scss';
 import SearchButton from '../SearchButton';
 import Translate from '@docusaurus/Translate';
 
 const UserNavbarDesktopItem = ({ authUrl, is_logged_in }: IUserNavbarItemProps) => {
   const [toggle_search, setToggleSearch] = useState<boolean>(false);
+  const serverUrl = localStorage.getItem('config.server_url');
 
-  const handleClick = () => {
-    location.assign(authUrl);
+  const handleClick = async () => {
+    // location.assign(authUrl);
+
+    const oidc = `https://${serverUrl}/.well-known/openid-configuration`;
+
+    try {
+      const response = await fetch(oidc);
+      const data = await response.json();
+
+      const endpoints = {
+        authorization_endpoint: data.authorization_endpoint,
+        token_endpoint: data.token_endpoint,
+        userinfo_endpoint: data.userinfo_endpoint,
+        end_session_endpoint: data.end_session_endpoint,
+      };
+
+      localStorage.setItem('config.oidc_endpoints', JSON.stringify(endpoints));
+
+      console.log(data);
+
+      const userManager = new UserManager({
+        authority: data.issuer,
+        client_id: '1011',
+        redirect_uri: 'http://localhost:3000/login/callback',
+        response_type: 'code',
+        scope: 'openid',
+        stateStore: new WebStorageStateStore({ store: window.localStorage }),
+        post_logout_redirect_uri: data.end_session_endpoint,
+        client_secret: '1011_secret',
+        metadata: {
+          issuer: data.issuer,
+          authorization_endpoint: data.authorization_endpoint,
+          token_endpoint: data.token_endpoint,
+          userinfo_endpoint: data.userinfo_endpoint,
+          end_session_endpoint: data.end_session_endpoint,
+        },
+      });
+
+      await userManager.signinRedirect();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const logInButtonClasses = clsx(
---
+++ src/pages/login/callback/index.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect } from 'react';
+import Layout from '@theme/Layout';
+
+export default function LoginCallback(): JSX.Element {
+  const [error, setError] = React.useState<string | null>(null);
+  const [error_description, setErrorDescription] = React.useState<string | null>(null);
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const code = urlParams.get('code');
+  const state = urlParams.get('state');
+
+  console.log(code);
+  const oidc_endpoints = localStorage.getItem('config.oidc_endpoints');
+
+  const token_endpoint = JSON.parse(oidc_endpoints).token_endpoint;
+
+  useEffect(() => {
+    const navbar = document.querySelector('.navbar.navbar--fixed-top') as HTMLElement;
+    if (navbar) {
+      navbar.style.display = 'none';
+    }
+
+    const exchangeToken = async () => {
+      try {
+        const oidc_key = `oidc.${state}`;
+
+        const oidc_data = localStorage.getItem(oidc_key);
+        const code_verifier = oidc_data ? JSON.parse(oidc_data).code_verifier : null;
+
+        console.log(code_verifier);
+
+        if (!code_verifier) return;
+
+        const response = await fetch(token_endpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Accept: 'application/json',
+          },
+          body: new URLSearchParams({
+            grant_type: 'authorization_code',
+            redirect_uri: 'http://localhost:3000/login/callback',
+            code: code,
+            code_verifier: code_verifier,
+            client_id: '1011',
+          }).toString(),
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+          console.log('Token exchange successful', data);
+          // Handle the access token here (e.g., save it or use it in further API calls)
+        } else {
+          console.error('Error exchanging token:', data);
+          setError(data.error);
+          setErrorDescription(data.error_description);
+        }
+      } catch (error) {
+        console.error('Token exchange failed:', error);
+      }
+    };
+
+    exchangeToken();
+  }, [code, state, token_endpoint]);
+
+  return (
+    <Layout>
+      {error && <p style={{ fontSize: '2rem', textAlign: 'center', color: 'red' }}>{error}</p>}
+      {error_description && (
+        <p style={{ fontSize: '1.5rem', textAlign: 'center', color: 'red' }}>{error_description}</p>
+      )}
+    </Layout>
+  );
+}

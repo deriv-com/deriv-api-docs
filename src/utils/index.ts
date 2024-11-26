6e7697d927a64f5d9e7ac1fd7f8aa2adaee7c331
muhammad-ahmed <muhammad.ahmed@regentmarkets.com>
2024-11-26 16:17:39 +0800

    ahmed/fix--prod-env-config-modification

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,6 +8,10 @@ import {
   VERCEL_DEPLOYMENT_APP_ID,
   OAUTH_URL,
   DEFAULT_WS_SERVER,
+  PRODUCTION_ME_APP_ID,
+  PRODUCTION_BE_APP_ID,
+  STAGING_ME_APP_ID,
+  STAGING_BE_APP_ID,
 } from './constants';
 
 const CURRENCY_MAP = new Map([
@@ -86,8 +90,12 @@ export const getAppId = () => {
   if (isHost('staging-api.deriv.com')) return STAGING_APP_ID;
   if (isHost('deriv-api-docs.binary.sx')) return VERCEL_DEPLOYMENT_APP_ID;
   if (isHost('api.deriv.com')) return PRODUCTION_APP_ID;
+  if (isHost('api.deriv.me')) return PRODUCTION_ME_APP_ID;
+  if (isHost('api.deriv.be')) return PRODUCTION_BE_APP_ID;
+  if (isHost('staging-api.deriv.me')) return STAGING_ME_APP_ID;
+  if (isHost('staging-api.deriv.be')) return STAGING_BE_APP_ID;
 
-  return VERCEL_DEPLOYMENT_APP_ID;
+  return PRODUCTION_APP_ID;
 };
 
 /**

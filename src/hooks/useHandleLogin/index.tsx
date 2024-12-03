58dcb8f33f602c91546698a0c27e6b922a204c83
Thisyahlen <thisyahlen@deriv.com>
2024-12-03 09:59:42 +0800

    chore: refactored login logic to hook

--- src/hooks/useHandleLogin/index.tsx
+++ src/hooks/useHandleLogin/index.tsx
@@ -9,12 +9,11 @@ import {
  * Handles the new login flow for the user using OIDC.
  *
  * If the user is not logged in and OAuth2 is enabled, it will redirect the user to the
- * OAuth2 authorization page. If the user is logged in, it will call the `onClickLogin`
- * callback.
+ * OAuth2 authorization page from the OIDC config endpoint. If OAuth2 is not enabled it will
+ * redirect the user to the legacy oauth url coming from the onClickLogin callback.
  *
  * @param {Object} props - The props object.
  * @param {Function} props.onClickLogin - The callback to be called when the user is logged in.
- *
  * @returns {Object} - An object with the `handleLogin` function.
  */
 export const useHandleLogin = ({ onClickLogin }: { onClickLogin: () => void }) => {

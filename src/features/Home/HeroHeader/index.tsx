5e2a12b122fdf0dab90bc354a8edb68578c6416d
muhammad-ahmed <muhammad.ahmed@regentmarkets.com>
2024-12-27 11:16:11 +0800

    fix internal routing on translation

--- src/features/Home/HeroHeader/index.tsx
+++ src/features/Home/HeroHeader/index.tsx
@@ -3,9 +3,26 @@ import { Button, Heading, Text } from '@deriv-com/quill-ui';
 import Translate from '@docusaurus/Translate';
 import styles from './HeroHeader.module.scss';
 import { useHistory } from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 const HeroHeader = () => {
   const history = useHistory();
+
+  const {
+    i18n: { currentLocale },
+  } = useDocusaurusContext();
+
+  const internal_links = React.useMemo(() => {
+    const is_en = currentLocale === 'en';
+    const get_url = (path: string) => {
+      const pathInfo = `${!is_en ? `/${currentLocale}` : ''}/${path}`;
+      return pathInfo;
+    };
+    return {
+      webSocketAPI: get_url('#WebsocketAPI'),
+      derivFixAPI: get_url('#derivFix'),
+    };
+  }, [currentLocale]);
 
   return (
     <div className={`${styles.heroContainer}`} data-testid='hero-header'>
@@ -19,10 +36,19 @@ const HeroHeader = () => {
           </Text>
         </div>
         <div className={styles.heroBtnContainer}>
-          <Button variant='primary' size='lg' onClick={() => history.push('/#WebsocketAPI')}>
+          <Button
+            variant='primary'
+            size='lg'
+            onClick={() => history.push(internal_links.webSocketAPI)}
+          >
             <Translate>WebSocket API</Translate>
           </Button>
-          <Button variant='secondary' color='white-black' size='lg' onClick={() => history.push('/#derivFix')}>
+          <Button
+            variant='secondary'
+            color='white-black'
+            size='lg'
+            onClick={() => history.push(internal_links.derivFixAPI)}
+          >
             <Translate>Deriv FIX API</Translate>
           </Button>
         </div>

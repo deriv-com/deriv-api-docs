8c850b40be2ab673dd6c069cdefe2b8b3e6017fd
muhammad-ahmed <muhammad.ahmed@regentmarkets.com>
2025-01-14 13:36:57 +0800

    ahmed/DAPI-817/chore-add-visual-effect-on-copy

--- src/features/dashboard/components/common-table/cell-copy-text.tsx
+++ src/features/dashboard/components/common-table/cell-copy-text.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LabelPairedCopyLgRegularIcon } from '@deriv/quill-icons';
 import { Text } from '@deriv-com/quill-ui';
 import styles from './cell-copy-text.module.scss';
@@ -8,23 +8,25 @@ const CopyTextCell: React.FC<{
     value: React.ReactNode;
   };
 }> = ({ cell }) => {
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(cell.value.toString());
+    setTooltipVisible(true);
+    setTimeout(() => setTooltipVisible(false), 1000);
+  };
+
   return (
     <React.Fragment>
       {cell.value ? (
-        <div
-          className={styles.copyText}
-          onClick={() => {
-            navigator.clipboard.writeText(cell.value.toString());
-          }}
-        >
+        <div className={styles.copyText} onClick={handleCopy}>
           <Text>{cell.value}</Text>
           <span className={styles.copyTextIcon}>
             <LabelPairedCopyLgRegularIcon />
+            {tooltipVisible && <div className={`${styles.tooltip} ${tooltipVisible ? styles.visible : ''}`}>Copied</div>}
           </span>
         </div>
-      ) : (
-        ''
-      )}
+      ) : ''}
     </React.Fragment>
   );
 };

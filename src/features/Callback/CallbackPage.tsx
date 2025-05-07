9a7b2ce08f789db4fadd9d3aeda261293546ad11
Rupato Braganza <rupato@deriv.com>
2025-05-07 16:29:48 +0800

    fix: api login flow

--- src/features/Callback/CallbackPage.tsx
+++ src/features/Callback/CallbackPage.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Callback } from '@deriv-com/auth-client';
 import { transformAccountsFromResponseBody } from '@site/src/utils';
 import useAuthContext from '@site/src/hooks/useAuthContext';
+import { useHistory } from '@docusaurus/router';
+
 const CallbackPage = () => {
   const { updateLoginAccounts } = useAuthContext();
+  const history = useHistory();
+
   return (
     <Callback
       onSignInSuccess={(tokens) => {
         const accounts = transformAccountsFromResponseBody(tokens);
         updateLoginAccounts(accounts);
-        window.location.href = '/';
+        history.push('/');
       }}
     />
   );
